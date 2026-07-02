420508a621751d89a9857ac702bf53592e1e113a
Elaine Cao <elainecao@Mac.lan>
2026-07-01 17:06:49 -0700

    Initial HealthMPV

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { supabase } from "./supabase";
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1>Health MPV</h1>
+      <p>My first AI Health App 🚀</p>
+    </div>
+  )
+}
+
+export default App;
+
